bb53cb8b1cef60189cccf494500fc147fcaaf866
ShauryaMalh <shaurya8141@gmail.com>
2025-03-05 17:14:27 -0800

    Somethings wrong

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# app.py
-from flask import Flask
+# main.py
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
+
--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 #__init.py
-from flask import Flask, app
+from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'WIP' #Encrypt/Secure our cookies and session data of our website
+    app.config['SECRET_KEY'] = 'WIP'  # Encrypt/Secure our cookies and session data of our website
+
+    from .views import views  # We need to register these blueprints in init.py
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
 
     return app
 
