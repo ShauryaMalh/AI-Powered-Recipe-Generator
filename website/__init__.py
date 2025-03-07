c31cc0bb3985643005a797dd9c66e37d89144434
ShauryaMalh <shaurya8141@gmail.com>
2025-03-06 17:04:40 -0800

    Funning local host

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,14 @@
-#__init.py
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'WIP'  # Encrypt/Secure our cookies and session data of our website
+    app.config['SECRET_KEY'] = 'WIP'  # Secure cookies and session data
 
-    from .views import views  # We need to register these blueprints in init.py
+    # Import after app is created to avoid circular imports
+    from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth')
 
     return app
-
-
