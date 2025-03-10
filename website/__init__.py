e45c7ef5a7080539cc1280b6f55f6b9381bae188
ShauryaMalh <shaurya8141@gmail.com>
2025-03-09 17:27:09 -0700

    Started base.html

--- website/__init__.py
+++ website/__init__.py
@@ -5,8 +5,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'WIP'  # Secure cookies and session data
 
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views) 
-
+    app.register_blueprint(auth)
 
     return app
